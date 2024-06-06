1568f0f4d3a4c4dec08806e26301f9098937aea4
tanvir244 <tr30365s25@gmail.com>
2024-06-07 03:36:06 +0600

    donation_requests get api created

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -46,6 +46,20 @@ async function run() {
       const result = await allRequestsCollection.insertOne(request);
       res.send(result);
     })
+
+    app.get('/donation_requests', async(req, res) => {
+      const query = {};
+      const options = {
+        projection: {recipient_name: 1, division: 1, district: 1, upazila: 1, donation_date: 1, donation_time: 1, donation_status: 1 }
+      }
+      const result = await allRequestsCollection.find( query, options).toArray();
+      res.send(result);
+    })
+
+    // app.get('/donation_requests', async(req, res) => {
+    //   const result = await allRequestsCollection.find().toArray();
+    //   res.send(result);
+    // })
 
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
